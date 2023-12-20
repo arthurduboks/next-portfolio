4919f52a6b6027b78535a6067e30b3a478afe5ec
Arthur Duboks <duboks.arthur@gmail.com>
2023-12-19 19:25:03 -0500

    switch color pallette for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[2rem] h-[20rem] w-[20rem] rounded-full blur-[5rem] sm:right-[11rem] sm:h-[31.25rem] sm:w-[31.25rem] sm:blur-[10rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-10rem] h-[20rem] w-[30rem] rounded-full blur-[5rem] sm:left-[-35rem] sm:h-[31.25rem] sm:w-[50rem] sm:blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <div className="bg-[rgb(251,226,227)] absolute -z-10 top-[-6rem] right-[2rem] h-[20rem] w-[20rem] rounded-full blur-[5rem] sm:right-[11rem] sm:h-[31.25rem] sm:w-[31.25rem] sm:blur-[10rem] dark:bg-[rgb(148,98,99)]"></div>
+        <div className="bg-[rgb(219,215,251)] absolute -z-10 top-[-1rem] left-[-10rem] h-[20rem] w-[30rem] rounded-full blur-[5rem] sm:left-[-35rem] sm:h-[31.25rem] sm:w-[50rem] sm:blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[rgb(103,99,148)]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
