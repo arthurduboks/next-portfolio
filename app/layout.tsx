951dfc83d1847f3f9be93a278f65928eece2bb30
Arthur Duboks <duboks.arthur@gmail.com>
2023-12-19 19:10:31 -0500

    optimize bg for mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,6 @@ import ThemeContextProvider from "@/context/theme-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const metadata = {
-  title: "Arthur Duboks - Full Stack React Developer, Montreal",
-  description:
-    "Arthur Duboks is a Full Stack Developer from Montreal with a deep focus on React, and its eco-system.",
-};
-
 export default function RootLayout({
   children,
 }: {
@@ -28,8 +22,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
-        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[2rem] h-[20rem] w-[20rem] rounded-full blur-[5rem] sm:right-[11rem] sm:h-[31.25rem] sm:w-[31.25rem] sm:blur-[10rem] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-10rem] h-[20rem] w-[30rem] rounded-full blur-[5rem] sm:left-[-35rem] sm:h-[31.25rem] sm:w-[50rem] sm:blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,15 @@ import Projects from "@/components/projects";
 import Skills from "@/components/skills";
 import Experience from "@/components/experience";
 import Contact from "@/components/contact";
+import { Metadata } from "next";
 
 import Divider from "@/components/divider";
+
+export const metadata: Metadata = {
+  title: "Arthur Duboks - Full Stack React Developer, Montreal",
+  description:
+    "Arthur Duboks is a Full Stack Developer from Montreal with a deep focus on React, and its eco-system.",
+};
 
 export default function Home() {
   return (
