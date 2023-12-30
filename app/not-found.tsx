5c67ed30096b056031f267beeb72097c4de6441d
Arthur Duboks <duboks.arthur@gmail.com>
2023-12-29 23:25:02 -0500

    404 page class update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function notFound() {
   return (
-    <section className="">
+    <section>
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">
